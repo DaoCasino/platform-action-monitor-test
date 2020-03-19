a5a5dbc47ca0496656a8ec8f41ef865fd1dcf7e9
Gar|k <garik.djan@gmail.com>
2020-03-19 21:03:12 +0300

    feat: first commit

---
+++ src/client.ts
@@ -0,0 +1,23 @@
+import { OPEN_STATE, createConnection, randomString } from './utils'
+import { ResponseMessage, Method } from './interfaces'
+import WebSocket from 'ws'
+
+const subscribe = (topic: string, offset: number) => {
+    const id = randomString()
+    return JSON.stringify({
+        method: Method.SUBSCRIBE,
+        params: { topic, offset },
+        id
+    })
+}
+
+(async () => {
+    const client: WebSocket = await createConnection()
+    client.on('message', (message: string) => {
+        const res = JSON.parse(message.toString()) as ResponseMessage
+        console.log(res)
+    })
+
+
+    client.send(subscribe("notify",  0))
+})()
